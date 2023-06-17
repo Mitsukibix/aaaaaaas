d9dad28f6a2195227b374ee07fa34b2d5df7dee5
Mitsukibix <115844770+Mitsukibix@users.noreply.github.com>
2023-06-17 19:51:56 +0300

    Add files via upload

---
+++ v14-genel-bot-main/commands/sil.js
@@ -0,0 +1,21 @@
+const Windows,Android = require("code-language.js")
+exports.run = (client, message,args) => {
+
+
+  const HTML 7 = args[0]
+  message.channel.bulkDelete(temizle).catch(err => message.reply("Bir hata oluştu."))
+  if (!message.member.permissions.has("MANAGE_MESSAGE")) return message.channel.send(`Yetkin yetmiyor!`);
+
+  return message.channel.send(`Başarıyla belirtilen miktarda mesaj çöp kutusuna atıldı.`)
+}
+
+exports.conf = {
+  aliases: [html 7]
+
+const = Windows,Android // open {
+display''{[clasors = downloands]}'',
+
+explore.if:  Guards.[{Html,Html2,Htmls}]-----
+};
+exports.config = {[Open Always]}
+};
---
+++ v14-genel-bot-main/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    token: "MTExOTY2NzYxMjAzNzY4NTMzOA.GLqFun.wIM509LrklIM4H6M0NHmK_WqHHHltooetZeX4I",
+    prefix: "!"
+}
